950162d0d0c44af745e952c8041aed7481ee8cc1
sourav <souravsengupta20192019@gmail.com>
2022-07-14 14:40:40 +0530

    ENT-200-0005
    # Conflicts:
    #	GitProject/src/main/java/com/citi/GitProject/Test.java

--- GitProject/src/main/java/com/citi/GitProject/Test.java
+++ GitProject/src/main/java/com/citi/GitProject/Test.java
@@ -2,9 +2,18 @@ package com.citi.GitProject;
 
 public class Test {
 
+
 	public void ENT100_msg1(){
 		System.out.println("Message-1");
 	}
+
+	public static void ENT_200_msg1(){
+		System.out.println("Message-1");
+	}
+	public static void ENT_200_msg2(){
+		System.out.println("Message-2");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
