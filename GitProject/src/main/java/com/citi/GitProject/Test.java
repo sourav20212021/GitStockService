5b97163911556875abd9ab7fa8ede50f551c5609
sourav <souravsengupta20192019@gmail.com>
2022-07-14 14:07:52 +0530

    ENT-100-0002

--- GitProject/src/main/java/com/citi/GitProject/Test.java
+++ GitProject/src/main/java/com/citi/GitProject/Test.java
@@ -2,6 +2,9 @@ package com.citi.GitProject;
 
 public class Test {
 
+	public void ENT100_msg1(){
+		System.out.println("Message-1");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
