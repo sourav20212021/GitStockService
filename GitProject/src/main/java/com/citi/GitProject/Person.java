159ba24d3504e763952927a92561c52d438eb15e
sourav <souravsengupta20192019@gmail.com>
2022-07-19 14:28:08 +0530

    msg-005

---
+++ GitProject/src/main/java/com/citi/GitProject/Person.java
@@ -0,0 +1,13 @@
+package com.citi.GitProject;
+
+public class Person {
+	public void test_1(){
+		System.out.println("Test Worlkd");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
