8b7cfe479be3eda4c06de52b2e9f2390ed06b0de
sourav <souravsengupta20192019@gmail.com>
2022-07-19 12:04:30 +0530

    19TH JULY-001

--- GitProject/src/main/java/com/citi/GitProject/Bonds.java
+++ GitProject/src/main/java/com/citi/GitProject/Bonds.java
@@ -2,6 +2,9 @@ package com.citi.GitProject;
 
 public class Bonds {
 
+	public void bond_1(){
+		System.out.println("Bond-1");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Bonds...");
