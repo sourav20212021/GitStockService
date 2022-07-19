74abc0d3d7ef6d2128df125b2ca8e0e1a7c75d44
sourav <souravsengupta20192019@gmail.com>
2022-07-19 15:00:45 +0530

    cr-001

--- GitProject/src/main/java/com/citi/GitProject/Person.java
+++ GitProject/src/main/java/com/citi/GitProject/Person.java
@@ -4,6 +4,9 @@ public class Person {
 	public void test_1(){
 		System.out.println("Test Worlkd");
 	}
+	public void test_2(){
+		System.out.println("Test Worlkd2");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- GitProject/src/main/java/com/citi/GitProject/Stock.java
+++ GitProject/src/main/java/com/citi/GitProject/Stock.java
@@ -8,6 +8,9 @@ public class Stock {
 	public void stock_2(){
 		System.out.println("Stocks-2");
 	}
+	public void stock_3(){
+		System.out.println("Stocks-3");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
